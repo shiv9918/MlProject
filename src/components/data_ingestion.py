0ad4ffdc9769db4ecb92aadc9dd71430aca9700b
shiv-tech001 <shivamtech0018@gmail.com>
2025-12-05 20:19:07 +0530

    model traning completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTranformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts',"train.csv")
@@ -31,7 +33,7 @@ class DataIngestion:
                 train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
 
                 train_set.to_csv(self.ingestion_config.train_data_path,index = False,header=True)
-                train_set.to_csv(self.ingestion_config.test_data_path,index = False,header=True)
+                test_set.to_csv(self.ingestion_config.test_data_path,index = False,header=True)
 
                 logging.info("Ingestion of the data is completed")
 
@@ -48,5 +50,8 @@ if __name__ == "__main__":
      train_data,test_data = obj.initiate_data_ingestion()
 
      data_transformation   = DataTransformation()
-     data_transformation.initiate_data_transformation(train_data,test_data)
+     train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+     modeltrainer = ModelTrainer()
+     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
                   
--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,8 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
-from src.exception import CustomException
+from .exception import CustomException
+from sklearn.metrics import r2_score
 def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -14,3 +15,26 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def evaluate_model(x_train, y_train, x_test, y_test, models):
+    try:
+        report = {}
+
+        for name, model in models.items():
+            model.fit(x_train, y_train)
+
+            y_train_pred = model.predict(x_train)
+            y_test_pred  = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score  = r2_score(y_test,  y_test_pred)
+
+            report[name] = {
+                "train_r2": train_model_score,
+                "test_r2":  test_model_score,
+            }
+
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
